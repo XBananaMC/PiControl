ccbf3b070dfeec21d29f91e0ff99311ba3133235
gerarddp <gerarddiazperlas@gmail.com>
2014-03-10 20:23:21 +0100

    Added navigation to logs

--- Beta/logs.php
+++ Beta/logs.php
@@ -1,13 +1,42 @@
 <?php
-	include 'header.php';	
+	include 'header.php';
+
+	$prev = $_REQUEST['p'];
 ?>
 	<div class="panel panel-default">
 		<div class="panel-body">
-			<table class="table">
 				<?php
 					//saveLog("INFO", "Gerard", "Open log");
+					if ($prev) $date = date('d-m-Y', strtotime(' -'.$prev.' day'));
+					else $date = date('d-m-Y', strtotime('today'));
+					$file = fopen("logs/".$date.".txt","r") or exit("Unable to open file!");
 
-					$file = fopen("logs/".date('d-m-Y', time()).".txt","r") or exit("Unable to open file!");
+				?>
+
+				<form action="logs.php" method="get" class="pull-left">
+					<input type="text" name="p" value="<?php echo $prev+1?>" hidden>
+					<input class="btn btn-default" type="submit" value="Previous">
+				</form>
+
+
+				<?php
+
+					if ($prev > 0) {
+						echo '<form action="logs.php" method="get" class="pull-right">
+							<input type="text" name="p" value="'.($prev-1).'" hidden>
+							<input class="btn btn-default" type="submit" value="Next">
+						</form>';
+					}
+
+				?>
+
+				<h3 class="text-center logs-header"><?php echo $date ?> </h3>
+				<br>
+
+				<table class="table">
+
+				<?php
+
 					$count = 0;
 					while(!feof($file)) {
 						$char = fgetc($file);
